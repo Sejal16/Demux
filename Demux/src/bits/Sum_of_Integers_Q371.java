2f861c8bc5cf6ffb723ede58a1a57c333818cd76
sejal <sejal.august16@gmail.com>
2021-02-02 21:26:59 +0530

    Medium

--- Demux/src/bits/Sum_of_Integers_Q371.java
+++ Demux/src/bits/Sum_of_Integers_Q371.java
@@ -10,61 +10,14 @@ Scanner sc = new Scanner(System.in);
 int a = sc.nextInt();
 int b=sc.nextInt();
 
-//if(a<0)
-//{
-//	a=~a;
-//	a=a+1;
-//}
-//if(b<0)
-//{
-//	b=~b;
-//	b=b|1;
-//}
-int ans=0,carry=0;
-for (int i = 0; i <32; i++) {
-	int p= a&(i<<1);
-	int q= b&(i<<1);
-	if(p==0 && q==0 )
-		if(carry==1)
-		{
-			ans=ans|(1<<i);
-			carry=0;
-		}
-		
-	else if( p==1 && q==0)
-		{
-		if(carry==1)
-		{
-			ans=ans|p;
-		carry=1;
-		}
-		else
-			carry=0;
-		}
-	else if(p==0 && q==1)
-		if(carry==1)
-		{
-		ans=ans|q;
-		carry=1;
-		}
-		else
-			carry=0;
-	else
-		{
-		if(carry==0)
-		{
-			ans|=0;
-		}
-		else
-		{
-		ans|=p;
-		
-		}carry=1;
-		}
-	
-	
+while(b!=0)
+{
+	int c=a&b; // Checking if any carry present or not
+	a=a^b;   /// find the sum without carry
+	b=c<<1;  /// placing the carry to its right position
 }
-System.out.println(ans);
+
+System.out.println(a);
 
 	}
 

5959839eb7c143cb06f95b8716fc2ec9523d9114
sejal <sejal.august16@gmail.com>
2021-02-19 21:48:16 +0530

    medium

---
+++ Demux/src/backtracking/Word_Search_Q79.java
@@ -0,0 +1,24 @@
+package backtracking;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Word_Search_Q79 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n=sc.nextInt();
+	 char arr[][]=new char[m][n];
+		for(int i=0;i<m;i++)
+			for(int j=0;i<n;i++)
+				arr[i][j]=sc.next().charAt(0);
+		List<List<Integer>> res=new ArrayList<>();
+		
+		
+		
+		
+				
+
+	}
+
+}
